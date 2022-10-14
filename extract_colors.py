a9a083b5adb580bb5a9986237bc876453cccddcd
Loïc Restoux <restouxloic@gmail.com>
2022-10-14 22:10:39 +0200

    Extracting colors (output in hexa)

---
+++ extract_colors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''
+Extracting colors as hexa
+'''
+
+# pip install Pillow
+from PIL import Image
+from os.path import splitext
+import sys
+
+if len(sys.argv) != 2:
+    print(f'Syntax: {sys.argv[0]} <image>')
+    exit(-1)
+file_input = sys.argv[1]
+
+input = Image.open(file_input)
+width, height = input.size
+pixels = input.load()
+
+for x in range(width):
+    for y in range(height):
+        colors = pixels[x, y]
+        print(f'{colors[0]:02x} {colors[1]:02x} {colors[2]:02x} ', end='')
+print()
